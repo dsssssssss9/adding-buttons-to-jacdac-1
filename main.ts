b1093068416b457de8f2eac2451d9dccc32a0b50
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-11 18:27:42 +0100

    Update pxt.json, main.blocks, main.ts, jacdac.json, jacdac.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
     basic.showNumber(1)
 })
+myModules.button3.onEvent(jacdac.ButtonEvent.Down, function () {
+    basic.showNumber(3)
+})
 modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
     basic.showNumber(2)
 })
